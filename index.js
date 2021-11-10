61dfe5bed5e2b7c0a18b41998dbb8af050417e6a
vla1 <vladis149@gmail.com>
2021-11-10 15:13:15 +0100

    show players

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-   let HISTORIC=[];
+let HISTORIC=[];
 var divRegiter = document.getElementById("UserGestrtaion");
 var divStar = document.getElementById("divStar");
 var divWin =   document.getElementById("btnWin");
 var inputValue = document.getElementById("inputStart");
 var divTable = document.getElementById("registration");
-
+var divHistory=document.getElementById("history");
 var beginingTime;
 
 var btnStart = document.getElementById("btnRegister");
@@ -17,18 +17,23 @@ btnStart.addEventListener("click" ,userName);
 btnStar.addEventListener("click" ,startGame);
 btnFinish.addEventListener("click", finish);
 
-function userName(score=0){
+function userName(){
     var name = inputValue.value;
-    newUser= {
+    HISTORIC=[];
+    var newUser= {
         name: name,
-        score: score
+        score: 0
     }
 if(inputValue.value=="")
 {}
 else{
+    if(localStorage.getItem("players")!=null){
+    HISTORIC.push(JSON.parse(localStorage.getItem("players")));
+    }
     HISTORIC.push(newUser);
+    console.log(HISTORIC);
     localStorage.setItem("players",JSON.stringify(HISTORIC));
-    var player = JSON.parse(localStorage.getItem("players"));
+   // var player = JSON.parse(localStorage.getItem("players"));
     divRegiter.className="hide";
     history();
     }
@@ -42,10 +47,20 @@ function startGame(){
 
 function history (){
     // console.log(HISTORIC[0])
-    var totalhistory = document.getElementById("history");
-    totalhistory.textContent=HISTORIC.toString();
+   // var totalhistory = document.getElementById("history");
+    //const list=document.createElement("li");
+    HISTORIC.forEach(i => {
+       createListElement({username:i.name, score:i.score});
+    });
+    divHistory.textContent=HISTORIC;
 }
 
+function createListElement({ username, score }) {
+    const newListItem = document.createElement("li");
+    newListItem.innerText = "User: " + username + "\nScore: " + score;
+    divHistory.appendChild(newListItem);
+  }
+  
 function finish(){
     var finalTime = Date.now();
     var gameTime = finalTime-beginingTime
@@ -53,14 +68,13 @@ function finish(){
 
 function positionDiv() {
     let positionLeft = Math.floor(Math.random()*90);
-    console.log(positionLeft);
+   // console.log(positionLeft);
     let positionTop = Math.floor(Math.random()*90);
     //let positionLeft = 25;
     //let positiontop = 25*0.65;
    
-    let coords =  divTable.getBoundingClientRect()
+   // let coords =  divTable.getBoundingClientRect()
     divWin.style.left = positionLeft+"%";
     divWin.style.top = positionTop+"%";
-  // divWin.setAttribute("style", "top:"+5*10+";left:"+positionLeft+";");
-    console.log(coords)
+    //console.log(coords)
 }
