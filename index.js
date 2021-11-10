c00b6ebd30fbb9f1864bdbbe2fe2c3b6b698ee22
vla1 <vladis149@gmail.com>
2021-11-10 11:06:18 +0100

    desplaza aleatorio

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
 
-const HISTORIC=[];
-var newUser;
+   let HISTORIC=[];
+var divRegiter = document.getElementById("UserGestrtaion");
+var divStar = document.getElementById("divStar");
+var divWin =   document.getElementById("btnWin");
 var inputValue = document.getElementById("inputStart");
-var btnStart = document.getElementById("btnStart");
-btnStart.addEventListener("click" ,()=>{
-    //var name=userName(inputValue.value)
-    //var lis= getLista();
-    //setLista(name);
-    addPlayerList(userName(inputValue.value));
-    //console.log(lis);
-});
-//obtener listado de jugadores del localStorage
-function getLista(){
-    const hitorical= JSON.parse(localStorage.getItem("userPlayer"));
-    return hitorical;
-}
-//vacia localStorage
-function clearStorage(){
-    localStorage.clear();
-}
-//crear listado jugadores en localStorage
-function setLista(lista){
-    localStorage.setItem("userPlayer",JSON.stringify(lista));
-}
-function addPlayerList(player){
-    var users=[];
-    const newUser=player;
-    
-    users.push(JSON.parse(localStorage.getItem("userPlayer")));
-    users.push(newUser);
-    console.log(users);
-    localStorage.setItem("userPlayer",JSON.stringify(users));
-  
-}
-//crear perfil jugador
-function userName(name,score=0){
-    return newUser= {
-         name: name,
-         score: score
-    };
+var divTable = document.getElementById("registration");
+
+var beginingTime;
+
+var btnStart = document.getElementById("btnRegister");
+var btnStar = document.getElementById("btnStar");
+var btnFinish = document.getElementById("btnFinish");
+
+divWin.addEventListener("click",positionDiv)
+btnStart.addEventListener("click" ,userName);
+btnStar.addEventListener("click" ,startGame);
+btnFinish.addEventListener("click", finish);
+
+function userName(score=0){
+    var name = inputValue.value;
+    newUser= {
+        name: name,
+        score: score
+    }
+if(inputValue.value=="")
+{}
+else{
+    HISTORIC.push(newUser);
+    localStorage.setItem("players",JSON.stringify(HISTORIC));
+    var player = JSON.parse(localStorage.getItem("players"));
+    divRegiter.className="hide";
+    history();
+    }
 }
 
+function startGame(){
+    beginingTime = Date.now();
+   /// console.log(beginingTime);
+    divStar.className="hide";
+}
 
+function history (){
+    // console.log(HISTORIC[0])
+    var totalhistory = document.getElementById("history");
+    totalhistory.textContent=HISTORIC.toString();
+}
 
+function finish(){
+    var finalTime = Date.now();
+    var gameTime = finalTime-beginingTime
+}
+
+function positionDiv() {
+    let positionLeft = Math.floor(Math.random()*90);
+    console.log(positionLeft);
+    let positionTop = Math.floor(Math.random()*90);
+    //let positionLeft = 25;
+    //let positiontop = 25*0.65;
+   
+    let coords =  divTable.getBoundingClientRect()
+    divWin.style.left = positionLeft+"%";
+    divWin.style.top = positionTop+"%";
+  // divWin.setAttribute("style", "top:"+5*10+";left:"+positionLeft+";");
+    console.log(coords)
+}
