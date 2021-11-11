be6e297446f5e4c2e17a7fd4872f5a2909ca90c9
JSSatorres <juansataz@gmail.com>
2021-11-11 16:14:15 +0100

    extra game time

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 var newUser;
 let HISTORIC=[];
-
+var beginingSore = 0;
+var positionVariation;
 //--------- Register step-----------
-var divRegister = document.getElementById("UserRegistration")
+var divRegister = document.getElementById("UserRegistration");
 var btnRegister = document.getElementById("btnRegister");
 btnRegister.addEventListener("click" ,userName);
 var inputValue = document.getElementById("inputRegister");
+var resultB = document.getElementById("resultB");
+
 
 //--------- Start step-----------
-var divStart = document.getElementById("divStart")
+var divStart = document.getElementById("divStart");
 var btnStart = document.getElementById("btnStart");
 btnStart.addEventListener("click" ,pauseGame);
 
 //--------- Game pause time-----------
 var divPause= document.getElementById("divPause");
 //--------- Game start step-----------
-var divWin =  document.getElementById("btnWin")
+var divWin =  document.getElementById("btnWin");
 var divTable = document.getElementById("registration");
 var divHistory=document.getElementById("history");
 var beginingTime;
 
 // ----- Finish Game
-divWin.addEventListener("click",finishGame)
+divWin.addEventListener("click",finishGame);
 var divRestart = document.getElementById("divRestart");
 var finishTime;
 var gameTime;
 
 //----- Restart Game
-var divResult = document.getElementById("result")
-var btnRestar =document.getElementById("restart")
-btnRestar.addEventListener("click",restart)
+var divResult = document.getElementById("result");
+var btnRestar =document.getElementById("restart");
+btnRestar.addEventListener("click",restart);
+
+//----- Start Game B -------------------
+var btnStartB = document.getElementById("btnStartB");
+btnStartB.addEventListener("click",startGameB);
+var divWinB =  document.getElementById("btnWinB");
+divWinB.addEventListener("click",clickscore)
+var divRestartB =document.getElementById("divRestartB");
+var visibleP =document.getElementById("visibleP");
+var visiPar = document.getElementById("visiPar");
 
 function userName(){
     var name = inputValue.value;
@@ -105,6 +117,10 @@ function finishGame(){
     divHistory.innerHTML="";
     history();
     divResult.innerHTML = gameTime ;
+    visibleP.className = "visible";
+    resultB.className = "hide";
+    result.className = "visible";
+    visiPar.className = "hide";
 }
 
 function restart(){
@@ -112,14 +128,51 @@ function restart(){
     divRegister.className="UserRegistration";
 }
 
+function startGameB(){
+    divWinB.className ="bTnWin";
+    //divPause.className="hide";
+    divStart.className="hide";
+    console.log("asdada")
+    positionVariation = setInterval(positionDivB,3000);
+    finishGameB()
+}
+
+function finishGameB() {
+    myVar = setTimeout(function(){
+        clearInterval(positionVariation)
+        console.log("amooos")
+        divWinB.className ="hide";
+        divRestart.className ="UserRegistration";
+        visibleP.className = "hide";
+        resultB.className = "visible";
+        result.className = "hide";
+        visiPar.className = "visible";
+        resultB.innerHTML = beginingSore;
+        console.log("btnRestar")
+    }, 3000);
+}
+
+function clickscore(){
+    beginingSore++;
+    console.log("beginingSore")
+}
+
+function positionDivB() {
+    let positionRight = Math.floor( Math.random()*90)
+    let positionTop =  Math.floor( Math.random()*90)
+        divWinB.style.right = positionRight + "%";
+        divWinB.style.top = positionTop + "%";
+}
 
 
+// var timeArray = setInterval (function(){ fruitaRandom()}, 3000);
 
 
-
-
-
-
+// function fruitaRandom(){
+// const fruitbomb= [1,2,3,4,5];
+// var randomArray = (Math.floor( Math.random()*4)+0);
+// var ramndomImage= fruitbomb[randomArray]
+// }
 
 
 
